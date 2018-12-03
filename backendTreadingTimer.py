0e7881a579eff3f9c38ba5a0d7fd6cb567e8544f
nicktrimborn <nick.trimborn@gmail.com>
2018-12-04 01:04:25 +0200

    Commented and cleaned up backendCode.  Adopted uniform style for .html files

--- backendTreadingTimer.py
+++ backendTreadingTimer.py
@@ -6,7 +6,6 @@ If the workstation has 6 pallets trigger an alarm - pi
 If the workstation is y amount of time idle, trigger an alarm - server
 If the workstation is x amount of time idle, trigger an alarm - server
 '''
-
 from flask import Flask, render_template
 from flask import request
 import json
@@ -17,13 +16,12 @@ import time
 #Import of the libraries for multithreading
 import threading
 
-#timeEachState = {"idle":0, "working":0, "error":0}
 ArrayStates=[]
 ArrayTimes=[]
 StatesProportions=[]
 
-timeX = 2
-timeY = 3
+timeX = 10
+timeY = 10
 timeExceeded = False
 
 serverAlarms = ["The system has been in Idle for "+str(timeX)+"seconds !",
@@ -32,7 +30,6 @@ serverAlarms = ["The system has been in Idle for "+str(timeX)+"seconds !",
 app = Flask(__name__)
 conn = sqlite3.connect(':memory:', check_same_thread=False)
 c = conn.cursor()
-
 
 c.execute("""CREATE TABLE workstation (
 	        Key INTEGER PRIMARY KEY,
@@ -87,13 +84,13 @@ def static_page(page_name):
 # Handler for post state from raspberry pie.  Message format eg. json={"state":"Working"}
 @app.route('/workstation/state', methods=['POST','GET'])
 def stateHandler():
-
     global timeEachState
     global timeExceeded
     global ArrayStates
     global ArrayTimes
     global StatesProportions
 
+    # updates total time for each state to SQL table
     def updateTotalTime(state, time1):
         with conn:
             c.execute("SELECT StateTotalTime FROM totalTime WHERE State=:State", {'State': state})
@@ -111,25 +108,6 @@ def stateHandler():
         # with conn:
         c.execute("INSERT INTO workstation VALUES (null, :State, :Time)", {'State': state,'Time':time})
 
-
-    #Function to compute the time spent on each state
-    def TimeSpentInEachState(TabStates, TabTimes):
-
-        #Initialize the time counters
-        W, I, E = 0, 0, 0
-
-        #Counting the time spent on each state
-        for i in range(len(TabStates)-1):
-            if TabStates[i]=="Working":
-                W = W + ((TabTimes[i + 1] - TabTimes[i]).seconds)
-            elif TabStates[i]=="Idle":
-                I = I + ((TabTimes[i + 1] - TabTimes[i]).seconds)
-            elif TabStates[i]=="Error":
-                E = E + ((TabTimes[i + 1] - TabTimes[i]).seconds)
-
-        #Return the times spent on each element (0:time spent in Working)
-        return [W, I, E, (W+I+E)]
-
     # Handle the GET and POST requests and update the HMI
     if request.method=='POST':
         content = request.json
@@ -139,30 +117,22 @@ def stateHandler():
         #time= now.isoformat()
         time= datetime.now().isoformat()
 
-
-
         ####### New Stuff
         if(getLastStateFromSQL()):
             currentState = getLastStateFromSQL()[0]
             if(currentState):
                 statetime = datetime.strptime(currentState[2],'%Y-%m-%dT%H:%M:%S.%f')
                 time1 = datetime.now() - statetime
-                totalTime = updateTotalTime(currentState[1],time1)
-
-        ####
-
-
-
-
+                updateTotalTime(currentState[1],time1)
+        #add new State to SQL table
         addStateSQL(newState, time)
+        #reply Message
         cnvMsg = {'state': newState, 'serverTime':time}
         cnvMsg_str = json.dumps(cnvMsg)
-        ArrayStates.append(newState)
-        ArrayTimes.append(datetime.strptime(time,'%Y-%m-%dT%H:%M:%S.%f'))
-        StatesProportions=(TimeSpentInEachState(ArrayStates, ArrayTimes))
-        #print(StatesProportions)
         timeExceeded = False
         return cnvMsg_str
+
+    # responds to get Requests
     elif request.method=='GET':
         #print ("(GET Request) Workstation State:")
         time = datetime.now().isoformat()
@@ -170,6 +140,7 @@ def stateHandler():
         cnvMsg_str = json.dumps(cnvMsg)
         return cnvMsg_str
 
+# This endpoint returns all states in the Workstation (state) SQL table
 @app.route('/workstation/allstates', methods= ['GET'])
 def statesHandler():
     c = conn.cursor()
@@ -179,32 +150,39 @@ def statesHandler():
     cnvMsg_str = json.dumps(cnvMsg)
     return cnvMsg_str
 
+# Endpoints to post and get current pallets in workstation (realtime)
 @app.route('/workstation/pallets', methods=['POST','GET'])
 def WorkstationHandler():
+
+    #Gets all pallets for Pallets SQL table (fixed with 6 rows)
     def get_Pallets():
         c = conn.cursor()
         c.execute("SELECT * FROM workstation_pallets")
         return c.fetchall()
 
+    # Updates the Table every time a change in pallets is sent from the Workstation (Pi)
     def updatePallets(PalletsArray):
         with conn:
             for i in range(6):
                 try:
                     c.execute("UPDATE workstation_pallets SET Content = :Contents WHERE Slot =:Index", {'Contents': PalletsArray[i], 'Index': i})
 
+                #Blanks in empty rows
                 except:
-                    c.execute("UPDATE workstation_pallets SET Content = :Contents WHERE Slot =:Index", {'Contents': 'Empty', 'Index': i})
-
-    if request.method=='POST':
+                    c.execute("UPDATE workstation_pallets SET Content = :Contents WHERE Slot =:Index", {'Contents': ' ', 'Index': i})
+    # Handler for posts from Workstation (Pi)
+    if request.method == 'POST':
         content = request.json
         PalletsArray=content["workstation"]
         #print(PalletsArray)
         updatePallets(PalletsArray)
         time= datetime.now().isoformat()
+        # standard reply message
         cnvMsg = {'workstation': 'empty','serverTime':time}
         cnvMsg_str = json.dumps(cnvMsg)
         return cnvMsg_str
-    elif request.method=='GET':
+    # Handler for get requests returns all pallets in Pallet SQL table
+    elif request.method == 'GET':
         cnvMsg = get_Pallets()
         cnvMsg_str = json.dumps(cnvMsg)
         #print(cnvMsg)
@@ -213,27 +191,32 @@ def WorkstationHandler():
 #Message format eg. json={"event":"Error State"}
 @app.route('/workstation/event', methods=['POST','GET'])
 def eventHandler():
+    # Function to insert alarms to events Table
     def addEventSQL(alID, alarmText, time):
         with conn:
             c.execute("INSERT INTO event VALUES (null, :AlarmID, :Event, :Time)", {'AlarmID': alID, 'Event': alarmText, 'Time':time})
 
+    # Returns all events from event SQL table
     def getEventFromSQL():
         c = conn.cursor()
         c.execute("SELECT * FROM event")
         return c.fetchall()
 
-    if request.method=='POST':
+    # Handles Post Requests from Workstation (Pi)
+    if request.method == 'POST':
         content = request.json
         #print ("(Post Req) New Event: " , content)
         alarmID = content["AlarmID"]
         AlarmText = content["AlarmText"]
         time = datetime.now().isoformat()
         addEventSQL(alarmID, AlarmText, time)
+        # Standard Reply to post request
         cnvMsg = {'alarmID': alarmID, 'event': AlarmText, 'serverTime':time}
         cnvMsg_str = json.dumps(cnvMsg)
         return cnvMsg_str
 
-    elif request.method=='GET':
+    # Returns all events on Get Requests
+    elif request.method == 'GET':
         #print ("(Get Req) Send WS Events:")
         cnvMsg = getEventFromSQL()
         cnvMsg_str = json.dumps(cnvMsg)
@@ -243,17 +226,21 @@ def eventHandler():
 #Message format eg. json={"counting"[spring.cylinder,valve, total]}
 @app.route('/workstation/historic', methods=['POST','GET'])
 def historyHandler():
+
+    #Adds pallet history to SQL table
     def addPalletHistorySQL(time, springCount, cylinderCount, valveCount, totalCount):
         with conn:
             c.execute("INSERT INTO pallet_history VALUES (:servertime , :SpringCount, :CylinderCount, :ValveCount, :TotalCount)",
                       {'servertime':time, 'SpringCount': springCount, 'CylinderCount': cylinderCount,'ValveCount':valveCount, 'TotalCount': totalCount})
 
+    # Returns pallet history from SQL table
     def getHistoryFromSQL():
         c = conn.cursor()
         c.execute("SELECT * FROM pallet_history")
         return c.fetchall()
 
-    if request.method=='POST':
+    # Handles posts from PI Containing running count of pallets from Workstation (Pi)
+    if request.method == 'POST':
         content = request.json
         #print ("(Post Req) Historic" , content)
         springCount = content["counting"][0]
@@ -266,6 +253,7 @@ def historyHandler():
         cnvMsg_str = json.dumps(cnvMsg)
         return cnvMsg_str
 
+    # Returns pallet history to get Request
     elif request.method=='GET':
         #print ("(Get Req) Send WS Events:")
         cnvMsg = getHistoryFromSQL()
@@ -273,56 +261,47 @@ def historyHandler():
         #print(cnvMsg)
         return cnvMsg_str
 
+# Endpoint to retrieve OEE for chart
 @app.route('/workstation/oeevalues', methods= ['GET'])
 def statesProportionHandler():
+
+    # Retrieve total time from SQL table
     def getTotatlTime():
         c = conn.cursor()
         c.execute("SELECT * FROM totalTime")
         totalTimes = c.fetchall()
-        print("Test: ", totalTimes)
+        #print("Test: ", totalTimes)
         return totalTimes
 
+    totalTimes = getTotatlTime()
+    cnvMsg_str = json.dumps(totalTimes)
+    return cnvMsg_str
     #print("In StatePorportionHandler")
     #cnvMsg = StatesProportions
     #print(cnvMsg)
     #cnvMsg_str = json.dumps(cnvMsg)
-    totalTimes = getTotatlTime()
     #StatesProportions = [totalTimes["Idle"],]
-
     #StatesProportions = getTotatlTime()
-    cnvMsg_str = json.dumps(totalTimes)
-    return cnvMsg_str
 
+# Function to check live alarms
 def checkElapsedTimeAlarms():
     global timeX
     global timeY
     global timeExceeded
-    #global timeExceededError
 
     def get_last_item(table, selectColumn, sortBy):
         c = conn.cursor()
         c.execute("SELECT "+selectColumn+" FROM "+table+" ORDER BY "+sortBy+" DESC LIMIT 1")
         return c.fetchall()
 
-    def get_last2_items(table, selectColumn, sortBy):
-        c = conn.cursor()
-        c.execute("select * from "+table+" where id=(select max(id)-1 from "+table+")")
-        return c.fetchall()
-
     def getLastStateSQL():
         return get_last_item('workstation','*','time')
 
-    def getLastTwoStatesSQL():
-        return get_last2_items('workstation', '*', 'time')
-
     try:
         lastStateSQL = getLastStateSQL()
-        #last2StateSQL = getLastTwoStatesSQL()
-        #print (last2StateSQL)
         currentServerTime = datetime.strptime(datetime.now().isoformat(),'%Y-%m-%dT%H:%M:%S.%f')
         lastStateTime = datetime.strptime(lastStateSQL[0][2],'%Y-%m-%dT%H:%M:%S.%f')
         timeInterval = currentServerTime - lastStateTime
-        #print (last2StateSQL)
 
         if (lastStateSQL[0][1] != "Working"):
             if (lastStateSQL[0][1] == "Idle") and (timeInterval.seconds > timeX) and not(timeExceeded):
@@ -341,11 +320,8 @@ def checkElapsedTimeAlarms():
 # Function to get the last state on the SQL table
 def getLastStateFromSQL():
     c = conn.cursor()
-    # c.execute("SELECT * FROM workstation WHERE State=:state", {'state': 'Idle'})
-    # c.execute("SELECT * FROM workstation WHERE 1")
     c.execute("SELECT * FROM workstation ORDER BY Key DESC LIMIT 1")
     return c.fetchall()
-
 
 def checkTotalTime():
     #print("test: ", getLastStateFromSQL())
@@ -353,7 +329,6 @@ def checkTotalTime():
         current_state = getLastStateFromSQL()[0]
         #print("test:", current_state)
         if(current_state):
-            #print("was here")
             now = datetime.now()
             stateTime = datetime.strptime(current_state[2],'%Y-%m-%dT%H:%M:%S.%f')
             time1 = now - stateTime
@@ -361,16 +336,15 @@ def checkTotalTime():
                 c.execute("SELECT StateTotalTime FROM totalTime WHERE State=:State", {'State': current_state[1]})
                 totalT = c.fetchone()
                 totalTime1 = totalT[0] + time1.seconds
-
                 c.execute("UPDATE totalTime SET DynTime=:DynTime WHERE State=:State",
                           {'DynTime': totalTime1, 'State':current_state[1]})
-            print("test:", totalTime1)
+            #print("test:", totalTime1)
 
+# Polling function to call server side functions for time exceeded alarms and time spent in each state
 def pollingUpdates():
     checkElapsedTimeAlarms()
     checkTotalTime()
     threading.Timer(0.5, pollingUpdates).start()
-
 
 if __name__ == '__main__':
     pollingUpdates()
